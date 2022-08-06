b42dc48f23bd60d14ae7bc55ae72617f44735fa1
min33sky <min33sky@naver.com>
2022-08-06 12:28:33 +0900

    prisma init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { GetServerSideProps } from 'next';
+import { prisma } from '../db/client';
+
+export default function Home({ data }: any) {
+  return (
+    <div>
+      <code>{data}</code>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const questions = await prisma.pollQuestion.findMany({});
+  return {
+    props: {
+      data: JSON.stringify(questions),
+    },
+  };
+};
